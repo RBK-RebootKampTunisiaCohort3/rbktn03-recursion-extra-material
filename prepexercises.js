88fdf88fcb288822e2a1a8caf0302d85e4016956
ranoua lachheb <ranoua22@gmail.com>
2020-03-23 11:48:17 +0100

    recursion

--- prepexercises.js
+++ prepexercises.js
@@ -9,7 +9,12 @@
 //  sum(4); // => 4 + 3 + 2 + 1 + 0 => 10
 //  sum(5); // => 5 + 4 + 3 + 2 + 1 + 0 => 15
 // HINT: We can rephrase 'the sum of n' as 'n plus the sum of n - 1'.
-
+function sum(n){
+	if ( n === 0){
+		return 0
+	}
+	    return n+sum(n-1)
+}
 // 2.Factorial of n: The factorial of n is the product of all the integers preceding n, starting with 1, e.g.
 
 //  function factorial(n) {
@@ -19,7 +24,12 @@
 //  factorial(4); // => 4 * 3 * 2 * 1 => 24
 //  factorial(5); // => 5 * 4 * 3 * 2 * 1 => 120
 // Implement the factorial function by observing that the 'factorial of n' can be rephrased as 'n times the factorial of n - 1'.
-
+function factorial(n){
+	if (n===1){
+		return 1
+	}
+	    return n*sum(n-1);
+}
 // 3.Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str, which is the string to be repeated, and count -- a number representing how many times the string str should be repeated, e.g.
 
 //  function repeatString(str, count) {
@@ -33,7 +43,14 @@
 //  we can concatenate that string onto the result of repeating the string count - 1.
 //  HINT: Observe that repeatString('dog', 0) should yield the empty string, ''.
 //  What happens if you evaluate this: ' ' + 'dog'?
-
+function reppeatstring(str,count){
+	if (count=== 0){
+		return ''
+	} else {
+	           str = str + reppeatstring(str,count-1)
+	           
+	return str 
+}
 // 4.Compute the nth Fibonacci Number: The fibonacci numbers are represented by the following sequence:
 
 //  // fib(n): 1 1 2 3 5 8 13 21
