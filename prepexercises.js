011c9e4ab381c086ec4e123645146ffe5c3da02a
mohamedBenAmmar <mr.weezy1312@gmail.com>
2020-03-23 11:11:34 +0100

    work in progress

--- prepexercises.js
+++ prepexercises.js
@@ -9,6 +9,13 @@
 //  sum(4); // => 4 + 3 + 2 + 1 + 0 => 10
 //  sum(5); // => 5 + 4 + 3 + 2 + 1 + 0 => 15
 // HINT: We can rephrase 'the sum of n' as 'n plus the sum of n - 1'.
+function sum (n) {
+	var sum=n;
+	for (var i=1; i<n ; i++){
+		sum+= i ;
+	}
+	return sum;
+}
 
 // 2.Factorial of n: The factorial of n is the product of all the integers preceding n, starting with 1, e.g.
 
@@ -19,6 +26,14 @@
 //  factorial(4); // => 4 * 3 * 2 * 1 => 24
 //  factorial(5); // => 5 * 4 * 3 * 2 * 1 => 120
 // Implement the factorial function by observing that the 'factorial of n' can be rephrased as 'n times the factorial of n - 1'.
+function factorial (n){
+	if (n<1) {
+		return 1;
+	}
+	else {
+		return n * factorial(n-1);
+	}
+}
 
 // 3.Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str, which is the string to be repeated, and count -- a number representing how many times the string str should be repeated, e.g.
 
@@ -33,6 +48,14 @@
 //  we can concatenate that string onto the result of repeating the string count - 1.
 //  HINT: Observe that repeatString('dog', 0) should yield the empty string, ''.
 //  What happens if you evaluate this: ' ' + 'dog'?
+function repeatString (str,count) {
+	if (count<1) {
+		return "";
+	}
+	else {
+		return str+repeatString(str,count-1);
+	}
+}
 
 // 4.Compute the nth Fibonacci Number: The fibonacci numbers are represented by the following sequence:
 
@@ -46,12 +69,28 @@
 //  fib(1) is 1
 //  fib(n) is fib(n - 1) + fib(n - 2)
 //  Write a function called fib that accepts a number n as a parameter and computes the nth fibonacci number using the above rules.
+function fibo (n){
+	if (n>1) {
+		return fibo(n-2) + fibo(n-1);
+	}
+	
+	else { return 1;
+
+	}
+}
 
 // 5.Write function that multiply the number by 10 n time
 
 //  multiplyBy10(number, n)
 //  multiplyBy10(4,3) => 4000
 //  multiplyBy10(5,2) => 500
+function multiply (number,n) {
+for (var i=0; i<n;i++){
+	number=number*10;
+}
+return number;
+}
+
 // ------------------------- More Practice -------------------------
 
 // 1.Modify your sum function from the Basic Requirements section to accept two parameters, start and end: sum should now compute the sum of the numbers from start to end, e.g.
@@ -62,10 +101,33 @@
 //  sum(2, 7); // => 2 + 3 + 4 + 5 + 6 + 7 => 27
 //  sum(3, 5); // => 3 + 4 + 5 => 12
 // What happens if start is larger than end? Modify sum to check for this case and, when found, swap the start and end arguments.
+function sum (a,b){
+var x=b;
+if (a<b){
+for (var i=a; i<b;i++){
+	x+=i;
+}}
+else {
+	return sum(b,a);
+}
+return x ;
+}
 
 // 2.Write a function product that works like sum, except it should compute the product of the numbers from start to end.
-
 // Refactor your sum function from earlier to be implemented in terms of product.
+
+function product (a,b){
+var x=b;
+if (a<b){
+for (var i=a; i<b;i++){
+	x*=i;
+}}
+else {
+	return sum(b,a);
+}
+return x ;
+}
+
 
 // 3.Let's pretend that JavaScript does not have the addition operator + -- instead, it comes with two functions called inc and dec that perform increment and decrement respectively:
 
@@ -78,7 +140,9 @@
 //  }
 // Your task is to write a function called add that takes two numbers as parameters, x and y, and adds them together.
 //  The catch is that you can only use inc and dec to accomplish this.
-
+function add ( x, y){
+	
+	}
 // 4.Write a function called isEven that, given a number n as a parameter, returns true if that number is even, and false otherwise; however, you need to do this without using the % operator
 
 // 5.Write a function called multiply that accepts two numbers as parameters, and multiplies them together -- but without using the * operator; instead, you'll need to use repeated addition.
