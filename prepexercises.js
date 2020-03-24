8d27bdd0c70e39387c97319db490a38c0c1c2a39
sami affes <sami_affes@yahoo.com>
2020-03-24 11:02:31 +0100

    nothing

--- prepexercises.js
+++ prepexercises.js
@@ -174,6 +174,17 @@ function multiply(x,y){
 //  range(1,9)   => '2, 3, 4, 5, 6, 7, 8'
 //  range(21,33) => '22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32'
 // Read about Increment and Decrement operators in JS
+
+function range(x,y){
+	if (x === y){
+        return x;
+      }
+  else if (x < y){
+  			return (x++,range(x,y--));
+  }
+  			return (y++,range(x--,y));
+}
+
 // ------------------------- Advanced -------------------------
 
 // 1.By now you should have worked with the length property of strings, e.g. 'hello'.length. Your task is to write a function called stringLength that accepts a string as a parameter and computes the length of that string; however, as you may have guessed, you are not allowed to use the length property of the string! Instead, you'll need to make use of the string method called slice. To get an idea of how slice works, try the following at a console:
